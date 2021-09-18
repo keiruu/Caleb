bfce47c6958dedbea0a7cfca8b38c45d0ab29282
Abigail Kaye Unating <abigailkaye.unating@wvsu.edu.ph>
2021-09-18 15:16:52 +0800

    Added getting announcements functionality
    - Getting announcements
    - Making a dictionary of courses to get ids for the announcements.

--- class/appClassroom.py
+++ class/appClassroom.py
@@ -6,22 +6,62 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly']
+SCOPES = ["https://www.googleapis.com/auth/classroom.announcements.readonly", "https://www.googleapis.com/auth/classroom.courses.readonly"]
 
-def getLsitOfCourses(service):
+courses = []
+coursesDict = {}
+
+def getListOfCourses(service):
     try:
         result = service.courses().list(pageSize=10).execute()
+
         listOfCourses = result.get('courses',[])
         
+        #create a dictionary with the courseId and courseName
+        #example: course_names = {'courseId': '275433710203', 'name': '2021-2-SS105E-BSIT2A'}
+        # A copy of a_dictionary is appended to a_list instead of appending a_dictionary directly 
+        # because that would produce a reference to a_dictionary instead of a copy.
+
+        for x in range(len(listOfCourses)):
+            coursesDict['courseId'] = listOfCourses[x].get("id")
+            coursesDict['name'] = listOfCourses[x].get("name")
+            coursesDict_copy = coursesDict.copy()
+            courses.append(coursesDict_copy)
+            
         if not listOfCourses:
             print("No list of Courses found.")
         else:
-            for course in listOfCourses:
-                print("CourseId : "+ course['name'] + "\nid : " + course['id'])
-                
+            print(listOfCourses)
 
     except errors.HttpError as error:
-        print(f"An error occured: {error}")
+        print("An error occured: {error}")
+
+def getAnnouncements(service):
+    try:
+        #code to get all announcements
+        resultDict = {}
+        result = service.courses().announcements().list(courseId=courses[9].get("courseId")).execute()
+        
+        #waay pa ni matapos, basically creating the message for 
+        #delivering the announcements to the user
+        for x in range(len(listOfCourses)):
+            result = service.courses().announcements().list(courseId=courses[x].get("courseId")).execute()
+            listOfAnnouncements = result.get('announcements',[])
+
+            if not result:
+                print("No announcements")
+            else:
+                print("Announcement: ", courses[x].get("name"))
+                listOfAnnouncements[0].get("text")
+
+        
+        if not listOfAnnouncements:
+            print("No list of Announcements found.")
+        else:
+            print(listOfAnnouncements)
+
+    except errors.HttpError as error:
+        print("An error occured: {error}")
 
 def getService():
     creds = None
